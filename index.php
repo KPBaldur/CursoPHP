0a4b326c5051a38c2b4c376ab124fbda2e25a6df
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-09-08 10:58:43 -0300

    Sintaxis basica

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
 
-//Hola mundo 
-echo "Hola, PHP!";
+//Hola mundo en PHP
+echo "Hola mundo en...... PHP";
 
-/*
-Este
-es
-un comentario
+/* Para poder realizar un salto de linea se debe agregar "\n"
+al final de una cadena de texto
 */
 
-//Creacion de variables
+/*Como crear una variable se inicia con un simbolo de 
+Dolar o de peso "$"
+*/
 
-$my_string = "Esto es una variable con una cadena de texto";
+/* Se puede utilizar un punto "." para poder concatenar cadenas de 
+texto*/
 
-//concatenar variables
+
+$my_string = "Esto es una cadena de texto";
+$my_string = "Aqui cambio el valor de la cadena de texto";
 echo $my_string . "\n";
+
+/*
+Para poder entregar la informacion sobre de que tipo es nuestra variable.
+*/
+echo gettype($my_string);
+
+/*
+Se puede cambiar de forma dinamica la variable asignada solo indicandole un
+valor nuevo
+*/
+$my_string = 6; //Aqui se cambia la variable con el tipado dinamico
+echo $my_string . "\n";
+echo gettype($my_string) . "\n"; //Aqui podemos ver que el tipo de variable cambio a integer
+
+$my_bool = true;
+echo $my_bool . "\n";
