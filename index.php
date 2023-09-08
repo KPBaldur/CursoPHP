a52bb09b460ae518a044cd19a35f6cf8dab51801
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-09-07 23:26:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//Hola mundo 
+echo "Hola, PHP!";
+
+/*
+Este
+es
+un comentario
+*/
+
+//Creacion de variables
+
+$my_string = "Esto es una variable con una cadena de texto";
+
+//concatenar variables
+echo $my_string . "\n";
